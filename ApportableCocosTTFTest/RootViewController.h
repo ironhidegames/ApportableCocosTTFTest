3b3423c16090e6efc79e2ce1c12f80628ee44516
Ruben Garat <ruben@ironhidegames.com>
2013-08-22 11:05:39 -0300

    Initial Commit

---
+++ ApportableCocosTTFTest/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ApportableCocosTTFTest
+//
+//  Created by Ironhide on 22/8/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ ApportableCocosTTFTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ApportableCocosTTFTest
+//
+//  Created by Ironhide on 22/8/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
